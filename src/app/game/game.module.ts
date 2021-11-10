bbda2fb560725bde27fc0d4a3d7bca130c17c043
unknown <lucinda.souchet@gmail.com>
2021-11-10 17:55:13 +0100

    feat: add app/modules/game/game.module, add game-form component

--- src/app/game/game.module.ts
+++ src/app/game/game.module.ts
@@ -1,20 +1,21 @@
+import { GameModule } from './../modules/game/game.module';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-
 import { IonicModule } from '@ionic/angular';
-
 import { GamePageRoutingModule } from './game-routing.module';
 
 import { GamePage } from './game.page';
 
 @NgModule({
+  declarations: [GamePage],
   imports: [
     CommonModule,
     FormsModule,
     IonicModule,
     GamePageRoutingModule,
+    GameModule,
   ],
-  declarations: [GamePage]
+  exports: [GamePage],
 })
 export class GamePageModule {}
